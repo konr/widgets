81178b23b2301874bc3f5a95fcc0082326b1162b
Konrad Scorciapino <konr@konr.mobi>
2013-08-10 23:10:25 -0300

    Initial widget. Need to define architecure. Why not CSP? :D

--- floating-box.el
+++ floating-box.el
@@ -42,41 +42,92 @@ a positive number of padding to the edge."
       (list (point) padding))))
 
 
+(defun time-widget ()
+  '(function (lambda () (format "Time: %s" (format-time-string "%Hh%M")))
+             face '((t (:background "black" :foreground "green")))))
+
+(defun calendar-widget ()
+  '(function (lambda () (shell-command-to-string "cal"))
+             face '((t (:background "maroon" :foreground "golden")))))
+
+(defun buffer-info-mode ()
+  '(function (lambda () (concat
+                           (propertize "*Current Major Mode*"
+                                       'font-lock-face `(:weight bold))
+                           "\n"
+                           (format "%s" (major-mode (current-buffer)))))
+             face '((t (:background "#9f0" :foreground "black")))))
+
+(defun love-widget ()
+  '(function (lambda () (format "< k+t 3"))
+             margin-right 15
+             face '((t (:background "red" :height 2.1 :foreground "white")))))
+
+(defun gen-gnu ()
+  )
 
 ;; Todo
 ;;; - Window width resizing
 ;;; - When line is large
 
-(defun create-popup ()
+(defvar floating-boxes nil)
+
+(defun create-popup (message i j face)
   (interactive)
   (save-excursion
     (goto-char (window-start))
+    (next-line i)
     (destructuring-bind (edge-pos edge-padding) (place)
       (if (= edge-pos (line-end-position))
           (let* ((overlay (make-overlay edge-pos edge-pos))
                  (first-line (line-number-at-pos (window-start)))
-                 (message "[ HUAHUEHUAHUE ]")
-                 (after-string
-                  (concat (make-string (- edge-padding (length message) 1) ?\ )
-                          (propertize message 'face 'yascroll:thumb-text-area))))
-            (put-text-property 0 1 'cursor t after-string)
-            (overlay-put overlay 'after-string after-string)
-            (overlay-put overlay 'window (selected-window))
-            (setq floating-boxes (cons overlay floating-boxes))
-            )
-        (let ((overlay (make-overlay edge-pos (1+ edge-pos)))
-              (display-string
-               (propertize " "
-                           'face 'yascroll:thumb-text-area
-                           'cursor t)))
-          (overlay-put overlay 'display display-string)
-          (overlay-put overlay 'window (selected-window))
-          (setq floating-boxes (cons overlay floating-boxes)))))))
+                 (len (- edge-padding (length message) j))
+
+                 (len (- edge-padding (length message) j))
+                 )
+            (let ((after-string
+                   (concat (make-string len ?\ )
+                           (propertize message 'face face))))
+              (put-text-property 0 1 'cursor t after-string)
+              (overlay-put overlay 'after-string after-string)
+              (overlay-put overlay 'window (selected-window))
+              (setq floating-boxes (cons overlay floating-boxes))
+              ))
+        (message "FIXME!!!!!!!!!!!!!!!")))))
+
+
+(defun create-popup-with-various-lines (widget start)
+  (interactive)
+  (let* ((target (funcall widget))
+         (fun (plist-get target 'function))
+         (face (plist-get target 'face))
+         (padding-h 2)
+         (padding-v 1)
+         (margin-top 1)
+         (margin-right (or (plist-get target 'margin-right) 3))
+         (splat (split-string (funcall fun) "\n"))
+         (size (apply #'max (mapcar #'length splat)))
+         (splat (mapcar (lambda (x) (concat
+                                       (make-string padding-h ? )
+                                       x (make-string (- size (length x)) ? )
+                                       (make-string padding-h ? )))
+                        (append (make-list padding-v " ") splat (make-list padding-v " ")))))
+    (loop for x in splat
+          and i = start then (1+ i)
+          doing (create-popup x (+ i margin-top) margin-right face)
+          finally return i)))
+
+(defun create-various-widgets ()
+  (let ((widgets '(time-widget calendar-widget buffer-info-mode love-widget))
+        (spacing 1))
+    (loop for widget in widgets
+          and pos = 0 then
+          (+ spacing (create-popup-with-various-lines widget pos)))))
 
 (defun redraw-popup (&rest bar)
   (destroy-popup)
   (message "Redrawing")
-  (create-popup))
+  (create-various-widgets))
 
 (defun add-or-remove-hooks (addp)
   (if addp
